91a29aa4776346cd034bef06f953d90c5807b0a3
jamesfining <james.fining@nbcuni.com>
2019-01-23 18:38:20 -0500

    added SegementationDescriptor Class, init self from hex string

--- scte/Scte35.py
+++ scte/Scte35.py
@@ -12,6 +12,9 @@ class Scte35:
     def parse_splice_event(self, b64_data):
         return SpliceEvent(b64_data)
 
+    def parse_segmentation_descriptor(self, hex_string):
+        return SegmentationDescriptor(hex_string)
+
 
 class SpliceNull:
     def __init__(self, bitarray_data):
@@ -44,8 +47,68 @@ class TimeSignal:
 
 
 class SegmentationDescriptor:
-    def __init__(self, bitarray_data):
-        None
+    def __init__(self, hex_string):
+        bitarray_data = bitstring.BitString(bytes=bytes.fromhex(hex_string))
+        new_descriptor = {}
+        new_descriptor["splice_descriptor_tag"] = bitarray_data.read("uint:8")
+        new_descriptor["descriptor_length"] = bitarray_data.read("uint:8")
+        if new_descriptor["splice_descriptor_tag"] not in [0, 1, 2, 3]:
+            self.log.info("Skipping Unsupported splice_descriptor_tag: " + str(new_descriptor["splice_descriptor_tag"]))
+        new_descriptor["identifier"] = bitarray_data.read("uint:32")
+        if new_descriptor["splice_descriptor_tag"] is 0:
+            # avail descriptor
+            None
+        elif new_descriptor["splice_descriptor_tag"] is 1:
+            # DTMF Descriptor
+            None
+        elif new_descriptor["splice_descriptor_tag"] is 2:
+            new_descriptor["is_segmentation_descriptor"] = True
+            new_descriptor["segmentation_event_id"] = bitarray_data.read("uint:32")
+            new_descriptor["segmentation_event_cancel_indicator"] = bitarray_data.read("bool")
+            # Reserved
+            bitarray_data.pos += 7
+            if new_descriptor["segmentation_event_cancel_indicator"] is False:
+                new_descriptor["program_segmentation_flag"] = bitarray_data.read("bool")
+                new_descriptor["segmentation_duration_flag"] = bitarray_data.read("bool")
+                new_descriptor["delivery_not_restricted_flag"] = bitarray_data.read("bool")
+                if new_descriptor["delivery_not_restricted_flag"] is False:
+                    new_descriptor["web_delivery_allowed_flag"] = bitarray_data.read("bool")
+                    new_descriptor["no_regional_blackout_flag"] = bitarray_data.read("bool")
+                    new_descriptor["archive_allowed_flag"] = bitarray_data.read("bool")
+                    new_descriptor["device_restrictions"] = bitarray_data.read("uint:2")
+                else:
+                    # Reserved
+                    bitarray_data.pos += 5
+                if new_descriptor["program_segmentation_flag"] is False:
+                    new_descriptor["component_count"] = bitarray_data.read("uint:8")
+                    loop_counter = new_descriptor["component_count"]
+                    new_descriptor["components"] = []
+                    while loop_counter > 0:
+                        new_component = {}
+                        new_component["component_tag"] = bitarray_data.read("uint:8")
+                        # Reserved
+                        bitarray_data.pos += 7
+                        new_component["pts_offset"] = bitarray_data.read("uint:33")
+                        new_descriptor["components"].append(new_component)
+                if new_descriptor["segmentation_duration_flag"] is True:
+                    new_descriptor["segmentation_duration"] = bitarray_data.read("uint:40")
+                new_descriptor["segmentation_upid_type"] = bitarray_data.read("uint:8")
+                new_descriptor["segmentation_upid_length"] = bitarray_data.read("uint:8")
+                # UPID Parsing needs more work
+                upid_length_bits = new_descriptor["segmentation_upid_length"] * 8
+                new_descriptor["segmentation_upid"] = bitarray_data.read("uint:" + str(upid_length_bits))
+                new_descriptor["segmentation_type_id"] = bitarray_data.read("uint:8")
+                new_descriptor["segmentation_message"] = scte35_enums.get_message(
+                    new_descriptor["segmentation_type_id"])
+                new_descriptor["segment_num"] = bitarray_data.read("uint:8")
+                new_descriptor["segments_expected"] = bitarray_data.read("uint:8")
+                if new_descriptor["segmentation_type_id"] in [52, 54]:
+                    new_descriptor["sub_segment_num"] = bitarray_data.read("uint:8")
+                    new_descriptor["sub_segments_expected"] = bitarray_data.read("uint:8")
+        elif new_descriptor["splice_descriptor_tag"] is 3:
+            # Time Descriptor
+            None
+        self.as_dict = new_descriptor
 
 
 class SpliceEvent(Scte35):
