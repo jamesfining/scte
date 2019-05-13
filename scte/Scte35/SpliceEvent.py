4312e0090663bef10c12535a2f46aff725298532
Andrew Burnheimer <andrew.burnheimer@nbcuni.com>
2019-05-13 17:54:39 -0400

    Handle importing from init_dict better, and serialize better, with testing

--- scte/Scte35/SpliceEvent.py
+++ scte/Scte35/SpliceEvent.py
@@ -8,13 +8,19 @@ from scte.Scte35.SpliceSchedule import SpliceSchedule
 
 
 class SpliceEvent:
-    def __parse_splice_descriptors(self, descriptor_loop_length, bitarray_data):
+    def __parse_splice_descriptors(self, descriptor_loop_length, bitarray_data, init_arr=None):
         splice_descriptors = []
         bytes_left = descriptor_loop_length
+        i=0
         while bytes_left > 0:
-            new_descriptor = SpliceDescriptor(bitarray_data).as_dict
+            new_descriptor = None
+            if init_arr:
+                new_descriptor = SpliceDescriptor.from_dict(init_arr[i])
+                i += 1
+            else:
+                new_descriptor = SpliceDescriptor(bitarray_data)
             # Descriptor Length does not include splice_descriptor_tag
-            bytes_left -= new_descriptor["descriptor_length"]+2
+            bytes_left -= new_descriptor.as_dict["descriptor_length"]+2
             splice_descriptors.append(new_descriptor)
         return splice_descriptors
 
@@ -22,7 +28,26 @@ class SpliceEvent:
         super(SpliceEvent, self).__init__()
 
         if init_dict:
+            # On first pass, take all the simple data parameters
             self.splice_info_section = init_dict
+
+            # Now, replace simple dict representations with actual objects
+            if init_dict["splice_command_type"] is 0:
+                self.is_splice_null = True
+                self.splice_info_section["splice_null"] = SpliceNull(None)
+            elif init_dict["splice_command_type"] is 4:
+                self.is_splice_schedule = True
+                self.splice_info_section["splice_schedule"] = SpliceSchedule.from_dict(init_dict["splice_schedule"])
+            elif init_dict["splice_command_type"] is 5:
+                self.is_splice_insert = True
+                self.splice_info_section["splice_insert"] = SpliceInsert.from_dict(init_dict["splice_insert"])
+            elif init_dict["splice_command_type"] is 6:
+                self.is_time_signal = True
+                self.splice_info_section["time_signal"] = TimeSignal.from_dict(init_dict["time_signal"])
+
+            if init_dict["descriptor_loop_length"] > 0:
+                self.splice_info_section["splice_descriptors"] = self.__parse_splice_descriptors(self.splice_info_section["descriptor_loop_length"], bitarray_data=None, init_arr=init_dict["splice_descriptors"])
+
             return
 
         decoded_data = None
@@ -38,7 +63,7 @@ class SpliceEvent:
         self.splice_info_section["table_id"] = bitarray_data.read("uint:8")
         self.splice_info_section["section_syntax_indicator"] = bitarray_data.read("bool")
         self.splice_info_section["private"] = bitarray_data.read("bool")
-        bitarray_data.pos += 2
+        bitarray_data.pos += 2 # Reserved
         self.splice_info_section["section_length"] = bitarray_data.read("uint:12")
         self.splice_info_section["protocol_version"] = bitarray_data.read("uint:8")
         self.splice_info_section["encrypted_packet"] = bitarray_data.read("bool")
@@ -48,6 +73,8 @@ class SpliceEvent:
         self.splice_info_section["tier"] = bitarray_data.read("hex:12")
         self.splice_info_section["splice_command_length"] = bitarray_data.read("uint:12")
         self.splice_info_section["splice_command_type"] = bitarray_data.read("uint:8")
+
+        # the pos pointer of bitarray_data will be mutated after the following constructors
         if self.splice_info_section["splice_command_type"] is 0:
             self.is_splice_null = True
             self.splice_info_section["splice_null"] = SpliceNull(bitarray_data)
@@ -65,6 +92,61 @@ class SpliceEvent:
         if self.splice_info_section["descriptor_loop_length"] > 0:
             self.splice_info_section["splice_descriptors"] = self.__parse_splice_descriptors(self.splice_info_section["descriptor_loop_length"], bitarray_data)
 
+    #
+    @property
+    def bitstring_format(self):
+        """
+        Return a formatted string representing the object for use by the bitstring library.
+
+        Note: This function only returns a string for the top-level SpliceEvent
+        object. Representative bitstring() functions should be called on all
+        constituent objects to make up a full serialized representation.
+        """
+        bitstring_format = 'uint:8=table_id,' \
+                           'bool=section_syntax_indicator,' \
+                           'bool=private,' \
+                           'uint:2=0,' \
+                           'uint:12=section_length,' \
+                           'uint:8=protocol_version,' \
+                           'bool=encrypted_packet,' \
+                           'uint:6=encryption_algorithm,' \
+                           'uint:33=pts_adjustment,' \
+                           'uint:8=cw_index,' \
+                           'hex:12=tier,' \
+                           'uint:12=splice_command_length,' \
+                           'uint:8=splice_command_type'
+        return bitstring_format
+
+
+    def serialize(self):
+        splice_info_section_begin_bs = bitstring.pack(fmt=self.bitstring_format, **self.splice_info_section)
+
+        splice_command_type_bs = None
+        if self.splice_info_section["splice_command_type"] is 0:
+            raise NotImplementedError('Can not interpret splice_null events')
+
+        elif self.splice_info_section["splice_command_type"] is 4:
+            raise NotImplementedError('Can not interpret splice_schedule events')
+
+        elif self.splice_info_section["splice_command_type"] is 5:
+            raise NotImplementedError('Can not interpret splice_insert events')
+
+        elif self.splice_info_section["splice_command_type"] is 6:
+            splice_command_type_bs = self.splice_info_section["time_signal"].serialize()
+
+        descriptor_loop_length_bs = bitstring.pack(fmt='uint:16=descriptor_loop_length', **self.splice_info_section)
+
+        splice_descriptors_bs = None
+        if self.splice_info_section["descriptor_loop_length"] > 0:
+            for splice_descriptor in self.splice_info_section["splice_descriptors"]:
+                splice_descriptors_bs += splice_descriptor.serialize()
+
+        return splice_info_section_begin_bs + splice_command_type_bs + descriptor_loop_length_bs + splice_descriptors_bs
+
+    @property
+    def hex_string(self):
+        a = self.serialize().hex.upper()
+        return a
 
     @classmethod
     def from_hex_string(cls, hex_string):
--- test.py
+++ test.py
@@ -10,18 +10,28 @@ from scte.Scte35 import SpliceEvent, SpliceDescriptor
 #Zero Length UPID
 test_string = "/DA6AAAAAsrbAP/wBQb+Bp8k9gAkAiJDVUVJAAAAAX//AAE07tIBDkVQMDAwMDExODk0OTU0AQUAONWP9g=="
 
-hex_descriptor = "021B43554549000000027FBF030C54564E413130303030303031300000"
-
 myEvent = SpliceEvent(test_string)
 
-print(myEvent.splice_info_section)
+print("event from b64 : " + str(myEvent.splice_info_section))
 
-#descriptorObject = SpliceDescriptor.from_hex_string(hex_descriptor)
 
-#print(descriptorObject.as_dict)
+hx = myEvent.hex_string
+print("hex of event from b64: " + hx)
 
-#print("serialized: " + descriptorObject.hex_string)
-#print("original:   " + hex_descriptor)
+myEvent = SpliceEvent.from_hex_string(hx)
+print("formed from hex: " + str(myEvent.splice_info_section))
+
+
+hex_descriptor = "021B43554549000000027FBF030C54564E413130303030303031300000"
+
+descriptorObject = SpliceDescriptor.from_hex_string(hex_descriptor)
+
+print(descriptorObject.as_dict)
+
+print("serialized: " + descriptorObject.hex_string)
+print("original:   " + hex_descriptor)
+
+
 
 descriptor_dictionary = {'splice_descriptor_tag': 2,
                          'descriptor_length': 27,
@@ -33,7 +43,7 @@ descriptor_dictionary = {'splice_descriptor_tag': 2,
                          'delivery_not_restricted_flag': True,
                          'segmentation_upid_type': 3,
                          'segmentation_upid_length': 12,
-                         'segmentation_upid': 26101077992648258395619471409,
+                         'segmentation_upid': b'EP9990118900',
                          'segmentation_type_id': 48,
                          'segmentation_message': 'Provider Advertisement Start',
                          'segment_num': 0,
@@ -41,4 +51,29 @@ descriptor_dictionary = {'splice_descriptor_tag': 2,
 
 descObject2 = SpliceDescriptor.from_dict(descriptor_dictionary)
 
-print("from dict:  "+descObject2.hex_string)
+print("hex desc from dict:  "+descObject2.hex_string)
+
+
+
+event_dictionary = {'table_id': 252, 'section_syntax_indicator': False, 'private': False, 'section_length': 72, 'protocol_version': 0, 'encrypted_packet': False, 'encryption_algorithm': 0, 'pts_adjustment': 183003, 'cw_index': 0, 'tier': '000', 'splice_command_length': 5, 'splice_command_type': 6, 'time_signal': {'time_specified_flag': True, 'pts_time': 3588538282}, 'descriptor_loop_length': 50,
+        'splice_descriptors': [ {'splice_descriptor_tag': 2,
+            'descriptor_length': 48,
+            'identifier': 1129661769,
+            'is_segmentation_descriptor': True,
+            'segmentation_event_id': 4294967295,
+            'segmentation_event_cancel_indicator': False,
+            'program_segmentation_flag': True,
+            'segmentation_duration_flag': True,
+            'delivery_not_restricted_flag': True,
+            'segmentation_duration': 1350020,
+            'segmentation_upid_type': 9,
+            'segmentation_upid_length': 12,
+            'segmentation_upid': b'EP9990118900',
+            'segmentation_type_id': 48,
+            'segmentation_message': 'Provider Advertisement Start',
+            'segment_num': 3,
+            'segments_expected': 3}]}
+
+eventObject2 = SpliceEvent.from_dict(event_dictionary)
+
+print("event from dict:  " + str(eventObject2.splice_info_section))
